bc5dca811243ce5be9c774fd2bd4bb68713b87d7
John Sanpe <sanpeqf@gmail.com>
2024-04-08 14:05:46 +0800

    initial commit
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

---
+++ main.c
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright(c) 2024 John Sanpe <sanpeqf@gmail.com>
+ */
+
+#include <bfdev.h>
+
+int main(int argc, const char *argv[])
+{
+    bfdev_log_info("helloworld\n");
+    return 0;
+}
