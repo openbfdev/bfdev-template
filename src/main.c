3fedd9053f145312d6348dca491841661270a919
John Sanpe <sanpeqf@gmail.com>
2024-05-07 15:53:23 +0800

    feat config: added initial config
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

---
+++ src/main.c
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright(c) 2024 John Sanpe <sanpeqf@gmail.com>
+ */
+
+#include <config.h>
+
+int
+main(int argc, const char *argv[])
+{
+    bfdev_log_notice("bfdev infomation:\n");
+    bfdev_log_notice("  version: %s\n", __bfdev_stringify(BFDEV_VERSION));
+    bfdev_log_notice("  name: %s\n", __bfdev_stringify(BFDEV_NAME));
+    bfdev_log_notice("  arch: %s\n", __bfdev_stringify(BFDEV_ARCH));
+    bfdev_log_notice("  commitid: %s\n", __bfdev_stringify(BFDEV_COMMITID));
+    bfdev_log_notice("  branch: %s\n", __bfdev_stringify(BFDEV_BRANCH));
+
+    return 0;
+}
